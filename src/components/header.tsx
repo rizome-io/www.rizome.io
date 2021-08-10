4fc9d9764b3315b6dab60f2f35c1795b1a0e539f
Andrej Novikov <jazzaiman@gmail.com>
2021-08-10 15:15:46 +0300

    feat: svg logo

---
+++ src/components/header.tsx
@@ -0,0 +1,60 @@
+/** @jsx jsx */
+import { jsx } from 'theme-ui'
+import React from "react"
+
+import { Link } from 'gatsby'
+import Logo from '../../assets/svg/logo_with_name.svg'
+import { AnchorLink } from 'gatsby-plugin-anchor-links'
+
+export const Header = () =>
+  <div sx={{
+    position: 'fixed',
+    width: '100%',
+    variant: 'styles.header',
+  }}>
+    <header
+      sx={{
+      display: 'flex',
+      alignItems: 'center',
+      mx: ['1em', '2em', '8em', '5em', '14em']
+    }}>
+    <Link
+      to="/"
+      sx={{
+        variant: 'styles.navlink',
+      }}>
+      <div sx={{ position: 'relative' }}>
+        <Logo sx={{ position: 'absolute', top: -10 }}/>
+      </div>
+    </Link>
+    <div sx={{ mx: 'auto' }} />
+    <AnchorLink
+      to="/#about"
+      sx={{
+        variant: 'styles.navlink',
+      }}>
+      About
+    </AnchorLink>
+    <AnchorLink
+      to="/#vision"
+      sx={{
+        variant: 'styles.navlink',
+      }}>
+      Vision
+    </AnchorLink>
+    <Link
+      to="https://lucid.rizome.io"
+      sx={{
+        variant: 'styles.navlink',
+      }}>
+      Pool
+    </Link>
+    <AnchorLink
+      to="/#contact"
+      sx={{
+        variant: 'styles.navlink',
+      }}>
+      Contacts
+    </AnchorLink>
+  </header>
+</div>
