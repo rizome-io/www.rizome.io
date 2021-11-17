c6a487cebc736bc6a03e0d64d74edda5faecf1d4
Andrej Novikov <jazzaiman@gmail.com>
2021-11-17 17:39:20 +0200

    feat: gtag trackingIds update

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,7 +23,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-gtag`,
       options: {
-        trackingIds: ['G-KCVXS0MSGY'],
+        trackingIds: ['G-ZGG0B11G45'],
       },
     },
     {
